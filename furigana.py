f81c5a5c5b1db560efae772aa57bb35a750736af
Linghao Zhang <zlhdnc1994@gmail.com>
2016-03-31 13:28:23 +0800

    fix backup

--- furigana.py
+++ furigana.py
@@ -1,14 +1,18 @@
 # -*- encoding:utf-8 -*-
-import sys
-import fileinput
+import os
 import re
+import sys
+import shutil
+import fileinput
 
 __author__ = 'Linghao Zhang'
 
 
 if __name__ == '__main__':
-    filenames = sys.argv[1:]
-    for filename in filenames:
-        for line in fileinput.input(inplace=True, backup='.bak'):
-            line = re.sub(r'\[(.*?)\]\{(.*?)\}', r'<ruby>\1<rp>（</rp><rt>\2</rt><rp>）</rp></ruby>', line)
-            print line,
+    for file in sys.argv[1:]:
+        shutil.copyfile(file, file+'.bak')
+    for line in fileinput.input(inplace=True, backup='.tmp'):
+        line = re.sub(r'\[(.*?)\]\{(.*?)\}', r'<ruby>\1<rp>（</rp><rt>\2</rt><rp>）</rp></ruby>', line)
+        print line,
+    for file in sys.argv[1:]:
+        os.remove(file+'.tmp')
